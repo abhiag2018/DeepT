558ab401cf864e7438dfab734fda1f134ad02029
Abhishek Agarwal <abhishek.agarwal@jax.org>
2020-08-19 18:20:32 -0400

    check reRun

--- process_fasta.py
+++ process_fasta.py
@@ -13,14 +13,15 @@ if __name__=="__main__":
     hg19 = prep.hg19
     p = prep.promoter
     e = prep.enhancer
+    reRun = prep.reRun
 
     if args.taskType=="pDNA" or args.taskType=="DNA":
-        if not os.path.exists(p['fa-out']):
+        if reRun or not os.path.exists(p['fa-out']):
             pt.generate_elem_fa(hg19, p['bed-path'], out_fa = p['fa-out'])
         prName,prLoc,prSeq,prOhc = pt.fasta_to_onehot(p['fa-out'],outp=p['dna-out'])
 
     if args.taskType=="eDNA" or args.taskType=="DNA":
-        if not os.path.exists(e['fa-out']):
+        if reRun or not os.path.exists(e['fa-out']):
             pt.generate_elem_fa(hg19, e['bed-path'], out_fa = e['fa-out'])
         enhName,enhLoc,enhSeq,enhOhc = pt.fasta_to_onehot(e['fa-out'],outp=e['dna-out'])
 
